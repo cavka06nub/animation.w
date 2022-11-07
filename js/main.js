79511b460613da2c95e648da74ff691c97e0e042
cavka06nub <you@example.com>
2022-11-07 14:14:45 +0400

    first

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var seyahet = [
+    ["Tebiet",
+        ["Deniz","Hava limani"],"Ismayilli","Almaniya"
+
+    ],
+    ["Aytac","Gultac","Sema","Nubar","Azima-Esmira"],
+    ["Revan","Sebuhi","Rza","Deyanet"],
+    [
+        "Sah dag",["Yay-Qis"],"Mandarin",
+    ],
+    ["","Baki."]
+
+];
+
+// console.log(seyahet[1][3][0])
+// console.log(seyahet[1][1][1])
+// console.log(seyahet[2][1][2])
+// console.log(seyahet[2][3][3])
+// console.log(seyahet[3][2][5])
+// console.log(seyahet[3][0][4])
+// console.log(seyahet[0][2][5])
+// console.log(seyahet[1][3][4])
+console.log(seyahet[3][2][0]+seyahet[0][0][1]+
+    seyahet[2][0][4]+
+    seyahet[2][1][5]+seyahet[1][2][2]+" "+seyahet[1][0][3]+seyahet[3][0][4]+seyahet[1][4][9]+seyahet[0][3][2]+" "+seyahet[1][3][0]+
+seyahet[1][1][1]+seyahet[2][1][2]+seyahet[2][3][3]+
+seyahet[3][2][5]+seyahet[3][0][4]+seyahet[0][2][5]+
+seyahet[1][3][4]+seyahet[4][1][4]
+
+)
